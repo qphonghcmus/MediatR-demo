aeeae53a50ec2c643a683163e0fe41a70852fe37
Quoc Phong <qphongqh@gmail.com.vn>
2021-06-04 10:19:16 +0700

    Add project files.

---
+++ Services/Wrappers/IRequestWrapper.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Wrappers
+{
+    public interface IRequestWrapper<T> : IRequest<Response<T>> { }
+    public interface IHandlerWrapper<Tin, TOut> : IRequestHandler<Tin, Response<TOut>> where Tin : IRequestWrapper<TOut> { }
+}
---
+++ WebApi/Infrastructure/UserIdPipe.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Services;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApi.Infrastructure
+{
+    public class UserIdPipe<TIn, TOut> : IPipelineBehavior<TIn, TOut>
+    {
+        private HttpContext httpContext;
+        public UserIdPipe(IHttpContextAccessor accessor)
+        {
+            httpContext = accessor.HttpContext;
+        }
+        public async Task<TOut> Handle(TIn request, CancellationToken cancellationToken, RequestHandlerDelegate<TOut> next)
+        {
+            //var userId = httpContext.User.Claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+            if(request is BaseRequest br)
+            {
+                br.UserId = "woof";
+            }
+            var result = await next();
+            if(result is Response<Car> carResponse)
+            {
+                carResponse.Data.Name += " Checked";
+            }
+            return result;
+        }
+    }
+}
